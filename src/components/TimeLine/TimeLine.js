21ba93e4839602e2797b222b8ed0ecde44b73ccc
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-15 22:05:29 -0500

    deploy

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
 
 const Timeline = () => {
- 
   return (
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      My name is Mahrokh. I am a computer programmer. More projects will be uploaded soon...
+        My name is Mahrokh. I am a computer programmer. I'm so passionate about
+        front-end development and software design. I love C++ and Java. My
+        interests outside work include hiking, making coffee, and listening to
+        podcasts.
       </SectionText>
-      </Section>
+    </Section>
   );
 };
 
