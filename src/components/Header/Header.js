8e6d8ccecdc2c91547a6d003e7e6febfca3f3ed3
mahrokh sadro <mahrokh.sadro@j-sas.com>
2025-04-03 22:58:03 -0400

    updating

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,18 +16,19 @@ import {
 const Header = () => (
   <Container>
     <Div1>
-      <Link href="/">
-        <a
-          style={{
-            display: "flex",
-            alignItems: "center",
-            color: "white",
-            marginBottom: "20px",
-          }}
-        >
-          <DiCssdeck size="3rem" />
-          <Span>Portfolio</Span>
-        </a>
+      <Link
+        href="/"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          color: "white",
+          marginBottom: "20px",
+        }}
+      >
+        {/* <a> */}
+        <DiCssdeck size="3rem" />
+        <Span>Portfolio</Span>
+        {/* </a> */}
       </Link>
     </Div1>
     <Div2>
