f96e2d0023a1616973d1335f30de6a90334c710f
mahrokh sadro <mahrokh.sadro@j-sas.com>
2025-04-03 23:47:44 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static export
+  distDir: "build", // Avoid conflicts by using a custom build directory
 };
 
 module.exports = nextConfig;
